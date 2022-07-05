f948ddfa8a4f005f4db10ddf43cbc57084b36ab4
Kang <kangjianhu@conflux-chain.org>
2022-07-05 17:24:39 +0800

    document generation

--- contracts/Controller.sol
+++ contracts/Controller.sol
@@ -11,10 +11,16 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./APPCoin.sol";
 import "./APICoin.sol";
 import "hardhat/console.sol";
+/**
+* @title Controller
+* @dev DApp developers can register their app through this controller.
+* An ERC777 contract will be deployed, which then will be used as a settlement contract between API consumer and API supplier.
+*/
 
 contract Controller is Ownable {
     UpgradeableBeacon public appUpgradeableBeacon;
     APICoin public apiProxy;
+    /** @dev APP_CREATED event */
     event APP_CREATED(address indexed addr, address indexed appOwner);
     uint256 public nextId;
     mapping(uint256=>address) public appMapping;
@@ -28,6 +34,11 @@ contract Controller is Ownable {
         appUpgradeableBeacon = new UpgradeableBeacon(address(appImpl));
         appUpgradeableBeacon.transferOwnership(msg.sender);
     }
+    /**
+    * @dev Create/register a DApp.
+    * An ERC777 contract will be deployed, which then will be used as a settlement contract between API consumer and API supplier.
+    * Caller's address will be used as the `appOwner` of the contract.
+    */
     function createApp(string memory name_, string memory symbol_) public {
         APPCoin app = APPCoin((address(new BeaconProxy(address(appUpgradeableBeacon), ""))));
         app.initOwner(address(this));
@@ -38,6 +49,7 @@ contract Controller is Ownable {
         nextId += 1;
         emit APP_CREATED(address(app), msg.sender);
     }
+    /** @dev List created DApp settlement contracts. */
     function listApp(uint offset, uint limit) public view returns (address[] memory, uint total){
         require(offset < nextId, 'invalid offset');
         if (offset + limit >= nextId) {
