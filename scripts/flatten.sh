ff4ffbd3a61156d984c4410fe6addfe22aeb44a7
Tigo9527 <68840946+Tigo9527@users.noreply.github.com>
2022-07-11 18:30:01 +0800

    0711/deploy testnet (#15)
    * deploy evm testnet
    
    * flatten.sh;
    
    Co-authored-by: Kang <kangjianhu@conflux-chain.org>

---
+++ scripts/flatten.sh
@@ -0,0 +1,11 @@
+declare -a arr=("APPCoin" "APICoin" "Controller")
+
+## now loop through the above array
+for name in "${arr[@]}"
+do
+   echo "$name"
+   hardhat flatten contracts/$name.sol > flatten/$name.txt
+done
+#  proxy
+hardhat flatten ./contracts/upgrade-test/MyERC1967.sol > flatten/MyERC1967Proxy.txt
+hardhat flatten ./contracts/upgrade-test/MyBeaconProxy.sol > flatten/MyBeaconProxy.txt
