212ef217bf158a2ad948ae21c338bbcc7c74fffb
Kang <kangjianhu@conflux-chain.org>
2022-07-20 10:49:56 +0800

    document

--- contracts/upgrade-test/MyBeaconProxy.sol
+++ contracts/upgrade-test/MyBeaconProxy.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
 
-// Use this contract to flatten BeaconProxy, and then verify on scan.
+/** Use this contract to flatten BeaconProxy, and then verify on scan. */
 contract MyBeaconProxy is BeaconProxy{
     constructor (address impl) BeaconProxy(impl, "") {
 
--- contracts/upgrade-test/MyERC1967.sol
+++ contracts/upgrade-test/MyERC1967.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
-// Use this contract to flatten ERC1967Proxy, and then verify on scan.
+/** Use this contract to flatten ERC1967Proxy, and then verify on scan. */
 contract MyERC1967 is ERC1967Proxy{
     constructor (address impl) ERC1967Proxy(impl, "") {
 
