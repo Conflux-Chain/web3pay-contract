bca2e528afc97e4a9a29d443f0a173246f50d2a5
Kang <kangjianhu@conflux-chain.org>
2022-07-25 18:44:41 +0800

    make app coin a 1155; and add description.

--- contracts/APPCoin.sol
+++ contracts/APPCoin.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC777/IERC777Recipient.sol";
-import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
+import "@openzeppelin/contracts/token/ERC777/IERC777.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/utils/introspection/IERC1820Registry.sol";
@@ -22,10 +23,13 @@ import "./AppConfig.sol";
  * - forceWithdraw
  * - freeze
  */
-contract APPCoin is ERC777, AppConfig, Pausable, Ownable, IERC777Recipient {
+contract APPCoin is ERC1155, AppConfig, Pausable, Ownable, IERC777Recipient {
     bytes32 private constant _TOKENS_RECIPIENT_INTERFACE_HASH = keccak256("ERC777TokensRecipient");
+    IERC1820Registry internal constant _ERC1820_REGISTRY = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
     address public apiCoin;
     address public appOwner;
+    string public name;
+    string public symbol;
     modifier onlyAppOwner() {
         require(msg.sender == appOwner, 'not app owner');
         _;
@@ -59,17 +63,19 @@ contract APPCoin is ERC777, AppConfig, Pausable, Ownable, IERC777Recipient {
         if (frozenMap[from] > 0) {
             revert('Account is frozen');
         }
-        _mint(from, amount,'','');
+        _mint(from, FT_ID, amount, 'ApiCoin received');
     }
     // prevent transfer
     function _beforeTokenTransfer(
         address operator,
         address from,
         address to,
-        uint256 amount
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
     ) internal override {
         require(from == address(0) || to == address(0) || msg.sender == owner(), 'Not permitted');
-        super._beforeTokenTransfer(operator, from, to, amount);
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
     }
     // -------- owner/bill manager operation -----------
     /**
@@ -96,8 +102,8 @@ contract APPCoin is ERC777, AppConfig, Pausable, Ownable, IERC777Recipient {
         }
     }
     /** @dev Charge fee*/
-    function charge(address account, uint256 amount, bytes memory data) public virtual onlyAppOwner whenNotPaused{
-        _burn(account, amount, "", data);
+    function charge(address account, uint256 amount, bytes memory /*data*/) public virtual onlyAppOwner whenNotPaused{
+        _burn(account, FT_ID, amount);
         totalCharged += amount;
         if (chargedMapping[account] == 0 && amount > 0) {
             // record this account at the first time of charging.
@@ -106,24 +112,14 @@ contract APPCoin is ERC777, AppConfig, Pausable, Ownable, IERC777Recipient {
         chargedMapping[account] += amount;
         if (frozenMap[account] > 1) {
             // refund
-            uint256 appCoinLeft = balanceOf(account);
-            _burn(account, appCoinLeft, "", "refund");
+            uint256 appCoinLeft = balanceOf(account, FT_ID);
+            _burn(account, FT_ID, appCoinLeft);
             IERC777(apiCoin).send(account, appCoinLeft, "refund");
             delete frozenMap[account];
             emit Withdraw(account, appCoinLeft);
         }
     }
     // -------- api consumer operation -----------
-    /**
-    * @dev Not permitted.
-    * @notice Prevent anyone from transferring app coin.
-    */
-    function burn(uint256 amount, bytes memory ) public override whenNotPaused{
-        if(amount >= 0){
-            revert('Not permitted');
-        }
-        super.burn(0, ""); // suppress warning
-    }
     /** @dev Used by an API consumer to send a withdraw request, API key related to the caller will be frozen. */
     function withdrawRequest() public whenNotPaused {
         require(frozenMap[msg.sender] == 0, 'Account is frozen');
@@ -135,8 +131,8 @@ contract APPCoin is ERC777, AppConfig, Pausable, Ownable, IERC777Recipient {
         require(frozenMap[msg.sender] != 1, 'Frozen by admin');
         require(frozenMap[msg.sender] > 0, 'Withdraw request first');
         require(block.number - frozenMap[msg.sender] > forceWithdrawDelay, 'Waiting time');
-        uint256 appCoinLeft = balanceOf(msg.sender);
-        _burn(msg.sender, appCoinLeft, "force withdraw", "");
+        uint256 appCoinLeft = balanceOf(msg.sender, FT_ID);
+        _burn(msg.sender, FT_ID, appCoinLeft);
         IERC777(apiCoin).send(msg.sender, appCoinLeft, "force withdraw");
         delete frozenMap[msg.sender];
         emit Withdraw(msg.sender, appCoinLeft);
@@ -161,7 +157,7 @@ contract APPCoin is ERC777, AppConfig, Pausable, Ownable, IERC777Recipient {
     }
     // -------------------------open zeppelin----------------------------
     constructor()
-        ERC777("", "", new address[](0)) {
+        ERC1155("") {
     }
     /**
      *  Called immediately after constructing through Controller contract.
@@ -170,12 +166,13 @@ contract APPCoin is ERC777, AppConfig, Pausable, Ownable, IERC777Recipient {
         require(owner() == address(0), 'Owner exists');
         _transferOwnership(owner_);
     }
-    function init(address apiCoin_, address appOwner_, string memory name_, string memory symbol_) public onlyOwner{
+    function init(address apiCoin_, address appOwner_, string memory name_, string memory symbol_, string memory uri_) public onlyOwner{
         require(apiCoin == address(0), 'Already initialized!');
 
         _ERC1820_REGISTRY.setInterfaceImplementer(address(this), _TOKENS_RECIPIENT_INTERFACE_HASH, address(this));
-        _name = name_;
-        _symbol = symbol_;
+        name = name_;
+        symbol = symbol_;
+        _setURI(uri_);
         apiCoin = apiCoin_;
         appOwner = appOwner_;
         forceWithdrawDelay = 3600;
@@ -197,6 +194,22 @@ contract APPCoin is ERC777, AppConfig, Pausable, Ownable, IERC777Recipient {
     }
 
     function balanceOfWithAirdrop(address owner) virtual view public returns (uint256 total, uint256 airdrop) {
-        return (balanceOf(owner), 0);
+        return (balanceOf(owner, FT_ID), 0);
+    }
+
+    function _mintConfig(
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) internal override virtual {
+        _mint(to,id,amount, data);
+    }
+    function _burnConfig(
+        address from,
+        uint256 id,
+        uint256 amount
+    ) internal override virtual {
+        _burn(from,id,amount);
     }
 }
