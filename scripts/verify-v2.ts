358a281c4aa8bd32af4395bf4094ba213f27eaf8
Kang <kangjianhu@conflux-chain.org>
2022-09-29 15:39:27 +0800

    docs and verify-v2.ts

--- scripts/verify-v2.ts
+++ scripts/verify-v2.ts
@@ -11,6 +11,7 @@ async function main() {
     }
     const {AppCoinV2, exchangeImpl, apiWeightTokenImpl, apiWeightFactoryImpl, vipCoinFactoryImpl,
         appFactoryImpl, appRegistryImpl, appImpl,
+        cardTemplateImpl, cardTrackerImpl, cardShopImpl,
         exchangeProxy, apiWeightFactoryProxy, vipCoinFactoryProxy, appFactoryProxy, appRegistryProxy}
         = JSON.parse(fs.readFileSync(DEPLOY_V2_INFO.replace(".json", `.chain-${chainId}.json`)).toString());
     await Promise.all([
@@ -21,6 +22,9 @@ async function main() {
             verifyContract('VipCoinFactory', vipCoinFactoryImpl),
             verifyContract('AppFactory', appFactoryImpl),
             verifyContract('AppRegistry', appRegistryImpl),
+            verifyContract('CardTemplate', cardTemplateImpl),
+            verifyContract('CardTracker', cardTrackerImpl),
+            verifyContract('CardShop', cardShopImpl),
 
             verifyContract('ERC1967Proxy', exchangeProxy),
             verifyContract('ERC1967Proxy', apiWeightFactoryProxy),
