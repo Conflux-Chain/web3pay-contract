180a998ae3d10bd3ecf8f67fd2ff9f816455cb7d
Kang <kangjianhu@conflux-chain.org>
2022-10-24 11:58:45 +0800

    use standard library.

--- contracts/v2/SolidProxy.sol
+++ contracts/v2/SolidProxy.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+import "@openzeppelin/contracts/proxy/Proxy.sol";
+import "@openzeppelin/contracts/utils/StorageSlot.sol";
 
 /** Non-upgradable proxy. */
-contract SolidProxy {
+contract SolidProxy is Proxy {
     /**
      * @dev Storage slot with the address of the current implementation.
      * This is the keccak-256 hash of "eip1967.proxy.implementation" subtracted by 1, and is
@@ -10,63 +12,11 @@ contract SolidProxy {
      */
     bytes32 internal constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
-    struct AddressSlot {
-        address value;
-    }
-
     constructor(address newImplementation) {
-        getAddressSlot(_IMPLEMENTATION_SLOT).value = newImplementation;
+        StorageSlot.getAddressSlot(_IMPLEMENTATION_SLOT).value = newImplementation;
+    }
+    function _implementation() internal override view virtual returns (address) {
+        return StorageSlot.getAddressSlot(_IMPLEMENTATION_SLOT).value;
     }
 
-    /**
-     * @dev Returns an `AddressSlot` with member `value` located at `slot`.
-     */
-    function getAddressSlot(bytes32 slot) internal pure returns (AddressSlot storage r) {
-        /// @solidity memory-safe-assembly
-        assembly {
-            r.slot := slot
-        }
-    }
-
-    /**
- * @dev Fallback function that delegates calls. Will run if no other
-     * function in the contract matches the call data.
-     */
-    fallback() external payable virtual {
-        _delegate();
-    }
-
-    /**
-     * @dev Fallback function that delegates calls. Will run if call data
-     * is empty.
-     */
-    receive() external payable virtual {
-        _delegate();
-    }
-
-    function _delegate() internal virtual {
-        address implementation = getAddressSlot(_IMPLEMENTATION_SLOT).value;
-        assembly {
-        // Copy msg.data. We take full control of memory in this inline assembly
-        // block because it will not return to Solidity code. We overwrite the
-        // Solidity scratch pad at memory position 0.
-            calldatacopy(0, 0, calldatasize())
-
-        // Call the implementation.
-        // out and outsize are 0 because we don't know the size yet.
-            let result := delegatecall(gas(), implementation, 0, calldatasize(), 0, 0)
-
-        // Copy the returned data.
-            returndatacopy(0, 0, returndatasize())
-
-            switch result
-            // delegatecall returns 0 on error.
-            case 0 {
-                revert(0, returndatasize())
-            }
-            default {
-                return(0, returndatasize())
-            }
-        }
-    }
 }
