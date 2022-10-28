0274111af1b168f6ed1282a409c32bd279b9f9fd
Kang <kangjianhu@conflux-chain.org>
2022-10-28 14:58:06 +0800

    app management

--- contracts/v2/AppRegistry.sol
+++ contracts/v2/AppRegistry.sol
@@ -25,7 +25,6 @@ contract AppRegistry is Initializable, AccessControlEnumerable {
     event Removed(address indexed app, address indexed operator);
 
     bytes32 public constant CREATOR_ROLE = keccak256("CREATOR_ROLE");
-    bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
 
     AppFactory public appFactory;
     bool public creatorRoleDisabled; // allow any one to create app
@@ -51,18 +50,7 @@ contract AppRegistry is Initializable, AccessControlEnumerable {
         exchanger = exchanger_;
         appFactory = appFactory_;
     }
-    function addApp(address app_) public onlyRole (MANAGER_ROLE){
-        _apps.set(app_, block.timestamp);
-    }
-    function removeApp(address app_) public onlyRole (MANAGER_ROLE){
-        _apps.remove(app_);
-    }
-    function clearAllApps() public onlyRole (MANAGER_ROLE){
-        for (uint i=0; i<_apps.length(); i++) {
-            (address addr, ) = _apps.at(i);
-            _apps.remove(addr);
-        }
-    }
+
     function setExchanger(ISwapExchange exchanger_) public {
         require(address(exchanger) == address(0), "already set");
         exchanger = exchanger_;
