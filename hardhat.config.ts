bc67b0d0170f9c0d8cf262420b37eaf3ffc10732
Bo QIU <35757521+boqiu@users.noreply.github.com>
2022-11-04 14:49:19 +0800

    add prod config for deploy (#74)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -51,6 +51,11 @@ const config: HardhatUserConfig = {
       url: "http://127.0.0.1:8545/",
       gasPrice: 1_000_000_000,
     },
+    net1030: {
+      url: "https://evm.confluxrpc.com",
+      accounts:
+          process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
